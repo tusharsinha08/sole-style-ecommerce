6605d1c8e88880887e267838bf93d731d695dff5
tusharsinha08 <121822994+tusharsinha08@users.noreply.github.com>
2026-05-27 04:31:12 +0600

    feat: add sign out functionality and success notification in UserAccount

--- front-end/src/Providers/AuthProvider.jsx
+++ front-end/src/Providers/AuthProvider.jsx
@@ -4,6 +4,7 @@ import {
     createUserWithEmailAndPassword, 
     getAuth, onAuthStateChanged, 
     signInWithEmailAndPassword, 
+    signOut, 
     updateProfile 
 } from "firebase/auth";
 import useAxiosPublic from "../hooks/useAxiosPublic";
@@ -34,6 +35,10 @@ const AuthProvider = ({ children }) => {
         })
     }
 
+    const signOutUser = () => {
+        return signOut(auth);
+    }
+
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
@@ -47,11 +52,14 @@ const AuthProvider = ({ children }) => {
                 .then(res => {
                     if (res.data.token) {
                         localStorage.setItem('access-token', res.data.token)
-                        setLoading(false)
                     }
+                    setLoading(false)
                 })
             }
-            
+            else {
+                localStorage.removeItem('access-token')
+                setLoading(false)
+            }
         })
         
         return () => unsubscribe();
@@ -62,7 +70,8 @@ const AuthProvider = ({ children }) => {
         loading,
         createUser,
         signInUser,
-        updateUserProfile
+        updateUserProfile,
+        signOutUser
     }
 
     return (

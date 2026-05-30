95a7b6a7179f34d352f612b492b936aeb1419146
tusharsinha08 <121822994+tusharsinha08@users.noreply.github.com>
2026-05-30 07:36:42 +0600

    fix: remove unnecessary whitespace in Navbar component

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ToggleDarkMode from './ToggleDarkMode';
 import { Link } from 'react-router-dom';
 import { FaRegCircleUser } from "react-icons/fa6";
-
+ 
 const Navbar = () => {
     const navOptions = <>
         <li><Link to={'/'}>Home</Link></li>
